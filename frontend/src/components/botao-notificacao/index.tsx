bd0087a076392649bf7c245140aa682d2600ad5d
avnercaleb <avnercaleb@hotmail.com>
2022-11-15 07:49:00 -0300

    Criação do primeiro componente

---
+++ frontend/src/components/botao-notificacao/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/icon.svg'
+import './style.css'
+
+function BotaoNotificacao() {
+
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default BotaoNotificacao
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import BotaoNotificacao from './components/botao-notificacao'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <BotaoNotificacao />
+      <BotaoNotificacao />
+    </>
   </React.StrictMode>
 )
