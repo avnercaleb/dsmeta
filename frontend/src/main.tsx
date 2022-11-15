463a4fb6eb1c4d91799644ef4ea0895dab0bbed7
avnercaleb <avnercaleb@hotmail.com>
2022-11-15 08:57:14 -0300

    finalização da pagina estatica

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import BotaoNotificacao from './components/botao-notificacao'
+import Header from './components/header'
+import SalesCard from './components/sales-card'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <>
-      <App />
-      <BotaoNotificacao />
-      <BotaoNotificacao />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>      
     </>
   </React.StrictMode>
 )
