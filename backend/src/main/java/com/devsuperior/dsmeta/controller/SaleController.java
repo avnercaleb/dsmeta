89c8dca5079297f08536b0904bfe50100934254f
avnercaleb <avnercaleb@hotmail.com>
2022-11-17 07:13:03 -0300

    Implementação do metodo de busca das vendas por datas

--- backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -1,12 +1,11 @@
 package com.devsuperior.dsmeta.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -20,7 +19,10 @@ public class SaleController {
 	private SaleService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Sale>> findSales(){
-		return ResponseEntity.status(HttpStatus.OK).body(service.findSales());
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			Pageable page){
+		return service.findSales(minDate, maxDate, page);
 	}
 }
